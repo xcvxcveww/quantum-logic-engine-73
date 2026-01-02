cad8fb789bed6372bec494ab7860efe9c4769538
xcvxcveww <rnkje0f4yl@mail10p.com>
2026-01-02 10:44:11 +0700

    fix: small typo in kernel logic

--- src/kernel_8xbh.ts
+++ src/kernel_8xbh.ts
@@ -4,3 +4,8 @@ const service_8xbh = <T>(args: T): T => { return args; };
 def run_task_cpix():
     """Subsystem version 476"""
     return "cpix".upper()
+
+// Update 2r2w
+def run_task_2r2w():
+    """Subsystem version 1569"""
+    return "2r2w".upper()
